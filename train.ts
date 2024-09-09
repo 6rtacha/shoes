522a6c72758deae6b145a213ce5898af24e65ba9
kevin <ahadjon1999bb@gmail.com>
2024-09-09 09:01:23 +0900

    feat: create routing and member controller, H2-task

--- train.ts
+++ train.ts
@@ -1,18 +1,38 @@
+// H2-TASK: 
+
+// Shunday function tuzing, unga string argument pass bolsin. 
+// Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+
+function getDigits(data: string) {
+    let result: string[] = [];
+    const dataArray = data.split("");
+    dataArray.filter((ele) =>{
+        if(!isNaN(+ele)) {
+            result.push(ele);
+        }
+    })
+    let finalResult: string = result.join("");
+    console.log(finalResult);
+}
+getDigits("m14i1t");
+
+
 // H-TASK: 
 
 // shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 // MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(listOfNumbers: number[]){
-    const newList = listOfNumbers.map((ele, index) => {
-        return ele > 0 ? ele : null;
-    });
-    const result = newList.join('');
-    console.log(result);
+// function getPositive(listOfNumbers: number[]){
+//     const newList = listOfNumbers.map((ele, index) => {
+//         return ele > 0 ? ele : null;
+//     });
+//     const result = newList.join('');
+//     console.log(result);
     
-}
+// }
 
-getPositive([1, -4, 2]);
+// getPositive([1, -4, 2]);
 
 // G-TASK: 
 
