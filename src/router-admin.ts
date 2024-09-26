51b81f87d7da11466986c79a7cc4275cd4872f1b
kevin <ahadjon1999bb@gmail.com>
2024-09-27 01:39:35 +0900

    feat: develop getUsers business logic

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -34,4 +34,5 @@ routerAdmin.post(
 );
 routerAdmin.post('/product/:id', restaurantController.verifyRestaurant, productController.updateChosenProduct);
 /** User */
+routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers)
 export default routerAdmin;
