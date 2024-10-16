17bcc5020342bf50b902bb298095414f9a41e264
kevin <ahadjon1999bb@gmail.com>
2024-10-17 01:50:18 +0900

    feat: develop token creation via jwt

--- src/controllers/member.contrtoller.ts
+++ src/controllers/member.contrtoller.ts
@@ -1,46 +1,49 @@
-import {Request, Response} from "express"; 
+import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors from "../libs/Errors";
+import AuthService from "../models/Auth.service";
 
 const memberService = new MemberService();
+const authService = new AuthService();
 
 const memberController: T = {};
 // REACT
 
 memberController.signup = async (req: Request, res: Response) => {
-    try {
-        console.log("signup");
-        console.log("body:", req.body);
-        const input: MemberInput = req.body,
-            result: Member = await memberService.signup(input);
-        //TODO: Tokens Authentication
+  try {
+    console.log("signup");
+    console.log("body:", req.body);
+    const input: MemberInput = req.body,
+      result: Member = await memberService.signup(input),
+      token = await authService.createToken(result);
+    //TODO: Tokens Authentication
 
-        res.json({member: result});
-    }
-    catch (err) {
-        console.log("Error, signup", err);
-        if(err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard)
-    }
+    res.json({ member: result });
+  } catch (err) {
+    console.log("Error, signup", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
 };
 
 memberController.login = async (req: Request, res: Response) => {
-    try {
-        console.log("login");
-        console.log("body:", req.body);
-        const input: LoginInput = req.body,
-            result = await memberService.login(input)
-        //TODO: Tokens Authentication
+  try {
+    console.log("login");
+    console.log("body:", req.body);
+    const input: LoginInput = req.body,
+      result = await memberService.login(input),
+      token = await authService.createToken(result);
+    console.log("toke:", token);
 
-        res.json({member: result});
-    }
-    catch (err) {
-        if(err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard)
-    }
+    //TODO: Tokens Authentication
+
+    res.json({ member: result });
+  } catch (err) {
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
 };
-
 
 export default memberController;
