22726954e196ac032fd54d417b166bb5076341c0
kevin <ahadjon1999bb@gmail.com>
2024-10-23 16:04:54 +0900

    feat: develop getProducts rest

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,56 +1,81 @@
+import { ProductStatus } from "../libs/enums/product.enum";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-import { Product, ProductInput, ProductUpdateInput } from "../libs/types/product";
+import {
+  Product,
+  ProductInput,
+  ProductInquiry,
+  ProductUpdateInput,
+} from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
-
+import { T } from "../libs/types/common";
 
 class ProductService {
-    private readonly productModel;
+  private readonly productModel;
 
-    constructor() {
-        this.productModel = ProductModel;
+  constructor() {
+    this.productModel = ProductModel;
+  }
+
+  /* SPA */
+  public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
+    const match: T = { productStatus: ProductStatus.PROCESS };
+
+    if (inquiry.productCollection)
+      match.productCollection = inquiry.productCollection;
+    if (inquiry.search) {
+      match.productName = { $regex: new RegExp(inquiry.search, "i") };
     }
 
-    /* SPA */
+    const sort: T =
+      inquiry.order === "productPrice"
+        ? { [inquiry.order]: 1 } // dynamic key hosil qilibberadi
+        : { [inquiry.order]: -1 };
 
-    /* SRR */
+    const result = await this.productModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        { $skip: (inquiry.page * 1 - 1) * inquiry.limit }, // nechta malumotni otkazib yuborish
+        { $limit: inquiry.limit * 1 }, // nechta malumot kerak bolsa           (bular pagination hosil qiladi)
+      ])
+      .exec();
 
-    public async getAllProducts(): Promise<Product[]> {
-        const result = await this.productModel
-            .find()
-            .exec();
-        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
-        return result;
+    return result;
+  }
+  /* SRR */
 
+  public async getAllProducts(): Promise<Product[]> {
+    const result = await this.productModel.find().exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
+    return result;
+  }
+
+  public async createNewProduct(input: ProductInput): Promise<Product> {
+    try {
+      return await this.productModel.create(input);
+    } catch (err) {
+      console.error("Error, model:createNewProduct:", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
+  }
 
-    public async createNewProduct(input: ProductInput): Promise<Product> {
-        try{
-            return await this.productModel.create(input);
-        } catch(err) {
-            console.error("Error, model:createNewProduct:", err);
-            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+  public async updateChosenProduct(
+    id: string,
+    input: ProductUpdateInput
+  ): Promise<Product> {
+    // string => ObjectId
+    id = shapeIntoMongooseObjectId(id);
+    const result = await this.productModel
+      .findOneAndUpdate({ _id: id }, input, { new: true })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
 
-        }
-    }
-
-    public async updateChosenProduct(
-        id: string,
-        input: ProductUpdateInput
-    ): Promise<Product> {
-        // string => ObjectId
-        id = shapeIntoMongooseObjectId(id);
-        const result = await this.productModel
-            .findOneAndUpdate({_id: id}, input, {new: true} )
-            .exec();
-        if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
-
-        return result;
-
-
-    }
+    return result;
+  }
 }
 
 export default ProductService;
