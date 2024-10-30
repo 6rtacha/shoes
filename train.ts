f61cfce015f372a473ca62dc86852cb4ef80a32a
kevin <ahadjon1999bb@gmail.com>
2024-10-31 03:45:05 +0900

    ZB-TASK

--- train.ts
+++ train.ts
@@ -1,16 +1,28 @@
+// ZB-TASK:
+
+// Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
+// MASALAN: randomBetween(30, 50) return 45
+
+function randomBetween(num: number, num1: number) {
+  let randomNumber = Math.floor(Math.random() * (num1 - num + 1)) + num;
+  return randomNumber;
+}
+const result = randomBetween(30, 50);
+console.log(result);
+
 // ZA-TASK:
 
 // Shunday function yozing, u array ichidagi objectlarni “age” qiymati boyicha sortlab bersin.
 // MASALAN: sortByAge([{age:23}, {age:21}, {age:13}]) return [{age:13}, {age:21}, {age:23}]
 
-function sortByAge(arr: any[]) {
-  const result = arr.sort(function (a, b) {
-    return a.age - b.age;
-  });
-  console.log(result);
-}
+// function sortByAge(arr: any[]) {
+//   const result = arr.sort(function (a, b) {
+//     return a.age - b.age;
+//   });
+//   console.log(result);
+// }
 
-sortByAge([{ age: 23 }, { age: 21 }, { age: 13 }]);
+// sortByAge([{ age: 23 }, { age: 21 }, { age: 13 }]);
 
 // Z-TASK:
 
